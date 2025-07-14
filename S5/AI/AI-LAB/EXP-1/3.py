704a4fe59b91016c2d504cec4bb08ef61d4e9b3d
ZayedBH <Zayedbinhassan35@gmail.com>
2025-07-14 11:18:32 +0530

    Create 3.py

---
+++ S5/AI/AI-LAB/EXP-1/3.py
@@ -0,0 +1,14 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+
+print("addition ",a+b)
+print("subtraction ",a-b)
+print("multiplication ",a*b)
+
+if b == 0:
+	print("error")
+else:
+	print("division ",a/b)
+
+print("exponential",a**b)
+print("floordivision",a//b)
