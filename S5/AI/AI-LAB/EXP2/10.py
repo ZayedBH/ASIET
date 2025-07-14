869bd7f56c9bd2cf5ddf6de2fe3be730cc40c0a7
ZayedBH <Zayedbinhassan35@gmail.com>
2025-07-14 11:32:23 +0530

    Update 10.py

--- S5/AI/AI-LAB/EXP2/10.py
+++ S5/AI/AI-LAB/EXP2/10.py
@@ -1,10 +1,14 @@
 def is_prime(number):
+  if number <= 1:
+    return False
+  for i in range(2, number):
+    if (number % i) == 0:
+      return False
+  return True
 
-    if number <= 1:
-        return False 
- 
-    for i in range(2, number):
-        if number % i == 0:
-            return False  
+num = int(input("Enter a number to check: "))
 
-    return True  
+if is_prime(num):
+  print(f"{num} is a prime number.")
+else:
+  print(f"{num} is not a prime number.")
